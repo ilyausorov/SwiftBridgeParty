bd83be6b94b0bfc6566fc69fce810a86ad49c667
ilya usorov <ilya.usorov@gmail.com>
2019-11-08 23:29:39 -0500

    Extended Loading Screen

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import ExtendedLoadingScreen from '../screens/ExtendedLoading/ExtendedLoadingScreen';
 
 export default createAppContainer(
   createSwitchNavigator({
+    ExtendedLoading: ExtendedLoadingScreen,
     Main: MainTabNavigator,
   })
 );
---
+++ screens/ExtendedLoading/ExtendedLoadingScreen.js
@@ -0,0 +1,36 @@
+import React, { useEffect } from 'react';
+import { View, Image, StatusBar, StyleSheet, Dimensions } from 'react-native';
+import { NavigationEvents } from 'react-navigation';
+
+const splashImage = require('../../assets/images/splash.png');
+const { width, height } = Dimensions.get('window');
+
+export default function ExtendedLoadingScreen(props) {
+  const { navigation } = props;
+
+  return (
+    <View style={styles.container}>
+      <NavigationEvents
+        onWillFocus={() => {
+          StatusBar.setBarStyle('light-content')
+          setTimeout(() => {
+            navigation.navigate('Main');
+          }, 2500)
+
+        }}
+      />
+      <Image style={{flex: 1, width: width, height: height}} resizeMode={'contain'} source={splashImage} />
+    </View>
+  );
+}
+
+ExtendedLoadingScreen.navigationOptions = {
+  header: null,
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
