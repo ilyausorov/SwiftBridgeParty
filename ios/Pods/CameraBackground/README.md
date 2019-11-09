f087e77b705ddc3e1a576348343d0dc2e4ea455a
ilya usorov <ilya.usorov@gmail.com>
2019-11-08 20:06:49 -0500

    Initial

---
+++ ios/Pods/CameraBackground/README.md
@@ -0,0 +1,88 @@
+# UIView+CameraBackground
+Show camera layer as a background to any UIView.
+
+[![Swift Version][swift-image]][swift-url]
+[![Build Status][travis-image]][travis-url]
+[![License][license-image]][license-url]
+[![CocoaPods Compatible](https://img.shields.io/cocoapods/v/CameraBackground.svg)](https://img.shields.io/cocoapods/v/CameraBackground.svg)  
+[![Platform](https://img.shields.io/cocoapods/p/CameraBackground.svg?style=flat)](http://cocoapods.org/pods/CameraBackground)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+## Features
+* Both **front and back** camera supported.
+* **Flash** modes: auto, on, off.
+* Countdown **timer**.
+* Tap to **focus**.
+* Pinch to **zoom**.
+
+<p align="center">
+<img src="screenshots/focus.png"> &nbsp; <img src="screenshots/countdown.png">
+</p>
+
+## Usage
+
+```swift
+view.addCameraBackground()
+// ...
+view.takeCameraSnapshot( {
+      // animate snapshot capture
+      self.view.alpha = 0
+      UIView.animate(withDuration: 1) { self.view.alpha = 1 }
+  },
+  completion: { (capturedImage, error) -> () in
+      self.view.freeCameraSnapshot() // unfreeze image
+      // ... handle capturedImage and error
+  }
+)
+// ...
+view.removeCameraBackground()
+```
+
+**Important:** Remember to add `NSCameraUsageDescription` to your `Info.plist`.
+
+### Layout
+
+You can change the location of the camera controls (flash, timer, and front/back camera selection) or hide them altogether:
+
+```swift
+view.addCameraBackground(
+   showButtons: true,
+   buttonMargins: UIEdgeInsets(top: 30, left: 10, bottom: 10, right: 10),
+   buttonsLocation: .left
+)
+```
+
+## Installation
+
+### CocoaPods:
+
+```ruby
+pod 'CameraBackground'
+```
+
+Legacy versions:
+
+| Swift version | CameraBackground version |
+| :---: | :--- |
+| 4.0 (Xcode 9.4) | `pod 'MiniLayout', '~> 1.2.1'`<br>`pod 'MultiToggleButton', '~> 1.7.1'`<br>`pod 'CameraBackground', '~> 1.4.1'` |
+| 3 | `pod 'MiniLayout', '~> 1.1.0'`<br>`pod 'MultiToggleButton', '~> 1.5.2'`<br>`pod 'CameraBackground', '~> 1.2'` |
+| 2.3 | `pod 'MiniLayout', '~> 1.0.1'`<br>`pod 'MultiToggleButton', '~> 1.4.0'`<br>`pod 'CameraBackground', '~> 1.0.2'` |
+
+### Manually:
+
+Copy `Sources/*` to your Xcode project.
+
+## Meta
+
+[@yonatsharon](https://twitter.com/yonatsharon)
+
+[https://github.com/yonat/CameraBackground](https://github.com/yonat/CameraBackground)
+
+[swift-image]:https://img.shields.io/badge/swift-5.0-orange.svg
+[swift-url]: https://swift.org/
+[license-image]: https://img.shields.io/badge/License-MIT-blue.svg
+[license-url]: LICENSE.txt
+[travis-image]: https://img.shields.io/travis/dbader/node-datadog-metrics/master.svg?style=flat-square
+[travis-url]: https://travis-ci.org/dbader/node-datadog-metrics
+[codebeat-image]: https://codebeat.co/badges/c19b47ea-2f9d-45df-8458-b2d952fe9dad
+[codebeat-url]: https://codebeat.co/projects/github-com-vsouza-awesomeios-com
---
+++ ios/Pods/CameraBackground/Sources/UIView+CameraBackground.swift
@@ -0,0 +1,342 @@
+//
+//  UIView+CameraBackground.swift
+//  Show camera input as a background to any UIView.
+//
+//  Created by Yonat Sharon on 11/1/15.
+//  Copyright (c) 2015 Yonat Sharon. All rights reserved.
+//
+
+import AVFoundation
+import MultiToggleButton
+import SweeterSwift
+import UIKit
+
+public extension UIView {
+    // MARK: - Public Camera Interface
+
+    /// Change the current camera background layer, e.g. when a user taps a camera on/off button.
+    @objc func toggleCameraBackground(
+        _ position: AVCaptureDevice.Position = .unspecified,
+        showButtons: Bool = true,
+        buttonMargins: UIEdgeInsets = .zero,
+        buttonsLocation: NSLayoutConstraint.Attribute = .top
+    ) {
+        if nil != cameraLayer {
+            removeCameraBackground()
+        } else {
+            addCameraBackground(position, showButtons: showButtons, buttonMargins: buttonMargins, buttonsLocation: buttonsLocation)
+        }
+    }
+
+    /// Remove camera background layer
+    @objc func removeCameraBackground() {
+        removeCameraControls()
+        cameraLayer?.removeFromSuperlayer()
+    }
+
+    /// Add camera background layer
+    @objc func addCameraBackground(
+        _ position: AVCaptureDevice.Position = .unspecified,
+        showButtons: Bool = true,
+        buttonMargins: UIEdgeInsets = .zero,
+        buttonsLocation: NSLayoutConstraint.Attribute = .top
+    ) {
+        let session = AVCaptureSession.stillCameraCaptureSession(position)
+        let cameraLayer = CameraLayer(session: session ?? AVCaptureSession())
+        if session == nil {
+            cameraLayer.backgroundColor = UIColor.black.cgColor
+        } else {
+            cameraLayer.videoGravity = AVLayerVideoGravity.resizeAspectFill
+        }
+
+        layer.insertBackgroundLayer(cameraLayer, name: theCameraLayerName)
+        if showButtons {
+            addCameraControls(margins: buttonMargins, location: buttonsLocation)
+        }
+    }
+
+    /// Take snapshot of the camera input shown in the background layer.
+    /// - Parameters:
+    ///   - onTime: action to perform when the timer completes countdown. E.g., make a click sound and/or show a flash.
+    ///   - completion: handle captured image.
+    @objc func takeCameraSnapshot(_ onTime: (() -> Void)?, completion: ((_ capturedImage: UIImage?, _ error: NSError?) -> Void)? = nil) {
+        guard let cameraLayer = cameraLayer else { return }
+        viewWithTag(theCountdownLabelTag)?.removeFromSuperview()
+        performWithTimer(timerInterval) {
+            onTime?()
+            cameraLayer.connection?.isEnabled = false // to freeze image
+            cameraLayer.captureStillImage { capturedImage, error in
+                cameraLayer.session?.stopRunning()
+                completion?(capturedImage, error)
+            }
+        }
+    }
+
+    /// Re-start streaming input from camera into background layer.
+    @objc func freeCameraSnapshot() {
+        cameraLayer?.connection?.isEnabled = true // to unfreeze image
+        cameraLayer?.session?.startRunning()
+        removeFocusBox()
+    }
+
+    /// The background layer showing camera input stream.
+    @objc var cameraLayer: AVCaptureVideoPreviewLayer? {
+        return layer.sublayerNamed(theCameraLayerName) as? AVCaptureVideoPreviewLayer
+    }
+
+    // MARK: - Private Camera Controls
+
+    private var device: AVCaptureDevice? {
+        return (cameraLayer?.session?.inputs.first as? AVCaptureDeviceInput)?.device
+    }
+
+    private func addCameraControls(margins: UIEdgeInsets = .zero, location: NSLayoutConstraint.Attribute = .top) {
+        // buttons panel
+        let panel = UIStackView(side: location)
+        panel.tag = thePanelViewTag
+        panel.tintColor = .white
+        addSubview(panel)
+        panel.translatesAutoresizingMaskIntoConstraints = false
+        constrain(panel, at: .top, diff: margins.top)
+        constrain(panel, at: .left, diff: margins.left)
+        constrain(panel, at: .right, diff: -margins.right)
+
+        // switch camera button
+        if AVCaptureDevice.devices(for: AVMediaType.video).count > 1 || UIDevice.isSimulator {
+            let cameraButton = UIButton.buttonWithImage(bundledCameraTemplateImage("camera-switch"), target: self, action: #selector(switchCamera))
+            cameraButton.tag = theSwitchButtonTag
+            panel.addArrangedSubview(cameraButton)
+        }
+
+        // timer button
+        let timerButton = MultiToggleButton(
+            image: bundledCameraTemplateImage("camera-timer"),
+            states: ["", "3s", "10s"],
+            colors: [nil, .cameraOnColor, .cameraOnColor, .cameraOnColor]
+        )
+        timerButton.tag = theTimerButtonTag
+        panel.addArrangedSubview(timerButton)
+
+        // flash button
+        let flashButton = MultiToggleButton(
+            image: bundledCameraTemplateImage("camera-flash"),
+            states: ["Off", "On", "Auto"],
+            colors: [nil, .cameraOnColor]
+        ) { sender in
+            self.setFlashMode(sender.currentStateIndex)
+        }
+        flashButton.tag = theFlashButtonTag
+        panel.addArrangedSubview(flashButton)
+        updateFlashButtonState()
+
+        for button in panel.arrangedSubviews {
+            button.layer.shadowOpacity = 1
+            button.layer.shadowOffset = .zero
+        }
+
+        // focus and zoom gestures - uses gesture subclass to make it identifiable when removing camera
+        addGestureRecognizer(CameraPinchGestureRecognizer(target: self, action: #selector(pinchToZoom)))
+        addGestureRecognizer(CameraTapGestureRecognizer(target: self, action: #selector(tapToFocus)))
+        device?.changeMonitoring(true)
+        NotificationCenter.default.addObserver(self, selector: #selector(removeFocusBox), name: .AVCaptureDeviceSubjectAreaDidChange, object: nil)
+    }
+
+    private func removeCameraControls() {
+        // remove focus and zoom gestures
+        gestureRecognizerOfType(CameraPinchGestureRecognizer.self)?.removeFromView()
+        gestureRecognizerOfType(CameraTapGestureRecognizer.self)?.removeFromView()
+        layer.sublayerNamed(theFocusLayerName)?.removeFromSuperlayer()
+        NotificationCenter.default.removeObserver(self, name: NSNotification.Name.AVCaptureDeviceSubjectAreaDidChange, object: nil)
+
+        // remove controls
+        viewWithTag(thePanelViewTag)?.removeFromSuperview()
+        viewWithTag(theCountdownLabelTag)?.removeFromSuperview()
+    }
+
+    private func updateFlashButtonState() {
+        if let device = device {
+            if let flashButton = viewWithTag(theFlashButtonTag) as? MultiToggleButton {
+                if device.hasFlash {
+                    flashButton.isHidden = false
+                    flashButton.currentStateIndex = device.flashMode.rawValue
+                } else {
+                    flashButton.isHidden = true
+                }
+            }
+        }
+    }
+
+    // MARK: - Action: Switch Front/Back Camera
+
+    @objc func switchCamera(_: UIButton) {
+        if let session = cameraLayer?.session {
+            var cameraPosition = AVCaptureDevice.Position.unspecified
+            if let input = session.inputs.first as? AVCaptureDeviceInput {
+                cameraPosition = input.device.position
+                session.removeInput(input)
+            }
+            session.addCameraInput(cameraPosition.opposite())
+            updateFlashButtonState()
+            removeFocusBox()
+        }
+    }
+
+    // MARK: - Action: Toggle Flash Mode
+
+    func setFlashMode(_ rawValue: NSInteger) {
+        if let device = device {
+            if device.hasFlash {
+                if let newMode = AVCaptureDevice.FlashMode(rawValue: rawValue) {
+                    device.changeFlashMode(newMode)
+                }
+            }
+        }
+    }
+
+    // MARK: - Action: Toggle Timer
+
+    var timerInterval: Int {
+        if let numberInTitle = (viewWithTag(theTimerButtonTag) as? UIButton)?.currentTitle?.trimmingCharacters(in: CharacterSet(charactersIn: " s")) {
+            return Int(numberInTitle) ?? 0
+        }
+        return 0
+    }
+
+    private func performWithTimer(_ interval: Int, block: @escaping () -> Void) {
+        if interval > 0 {
+            let countdownLabel = CoundownLabel(seconds: interval, action: block)
+            countdownLabel.tag = theCountdownLabelTag
+            addConstrainedSubview(countdownLabel, constrain: .centerX, .centerY)
+        } else {
+            block()
+        }
+    }
+
+    // MARK: - Action: Pinch to Zoom
+
+    @objc func pinchToZoom(_ sender: UIPinchGestureRecognizer) {
+        enum Static {
+            static var initialZoom: CGFloat = 1
+        }
+        if let device = device {
+            if sender.state == .began {
+                Static.initialZoom = device.videoZoomFactor
+            }
+            device.changeZoomFactor(sender.scale * Static.initialZoom)
+        }
+    }
+
+    // MARK: - Action: Tap to Focus
+
+    @objc func tapToFocus(_ sender: UITapGestureRecognizer) {
+        let focusPoint = sender.location(in: self)
+
+        if let device = device {
+            if !device.isFocusPointOfInterestSupported && !device.isExposurePointOfInterestSupported {
+                return
+            }
+
+            let interestPoint = CGPoint(x: (focusPoint.y - bounds.minY) / bounds.height, y: 1 - (focusPoint.x - bounds.minX) / bounds.width)
+            device.changeInterestPoint(interestPoint)
+            showFocusBox(focusPoint)
+        } else if UIDevice.isSimulator {
+            showFocusBox(focusPoint)
+        }
+    }
+
+    private func showFocusBox(_ center: CGPoint) {
+        cameraLayer?.sublayerNamed(theFocusLayerName)?.removeFromSuperlayer()
+        let focusLayer = FocusBoxLayer(center: center)
+        focusLayer.name = theFocusLayerName
+        cameraLayer?.addSublayer(focusLayer)
+    }
+
+    @objc func removeFocusBox() { // not private because it is a selector for AVCaptureDeviceSubjectAreaDidChangeNotification
+        cameraLayer?.sublayerNamed(theFocusLayerName)?.removeFromSuperlayer()
+        if let device = device {
+            let interestPoint = device.isFocusPointOfInterestSupported ? device.focusPointOfInterest : device.exposurePointOfInterest
+            let center = CGPoint(x: 0.5, y: 0.5)
+            if !interestPoint.equalTo(center) {
+                device.changeInterestPoint(center)
+            }
+        }
+    }
+}
+
+class CameraLayer: AVCaptureVideoPreviewLayer {
+    override init(session: AVCaptureSession) {
+        super.init(session: session)
+        setup()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setup()
+    }
+
+    override init(layer: Any) {
+        super.init(layer: layer)
+        setup()
+    }
+
+    private func setup() {
+        observeNotification(name: UIDevice.orientationDidChangeNotification, selector: #selector(updateCameraFrameAndOrientation))
+    }
+
+    @objc func updateCameraFrameAndOrientation() {
+        guard let superlayer = superlayer else { return }
+        frame = superlayer.bounds
+        guard let connection = connection, connection.isVideoOrientationSupported,
+            let appOrientation = AVCaptureVideoOrientation(rawValue: UIApplication.shared.statusBarOrientation.rawValue)
+        else { return }
+        connection.videoOrientation = appOrientation
+    }
+}
+
+extension CALayer {
+    func sublayerNamed(_ name: String) -> CALayer? {
+        guard let sublayers = sublayers else { return nil }
+        for sublayer in sublayers {
+            if let sName = sublayer.name {
+                if sName == name {
+                    return sublayer
+                }
+            }
+        }
+        return nil
+    }
+
+    func insertBackgroundLayer(_ layer: CALayer, name: String? = nil) {
+        layer.frame = bounds
+        insertSublayer(layer, at: 0)
+        if let name = name {
+            layer.name = name
+        }
+    }
+}
+
+// MARK: - Private Constants
+
+private let thePanelViewTag = 98765
+private let theSwitchButtonTag = thePanelViewTag + 1
+private let theFlashButtonTag = thePanelViewTag + 2
+private let theTimerButtonTag = thePanelViewTag + 3
+private let theCountdownLabelTag = thePanelViewTag + 4
+private let theCameraLayerName = "camera"
+private let theFocusLayerName = "focusSquare"
+
+// MARK: - Identifiable Gesture Recognizers
+
+class CameraTapGestureRecognizer: UITapGestureRecognizer, UIGestureRecognizerDelegate {
+    override init(target: Any?, action: Selector?) {
+        super.init(target: target, action: action)
+        cancelsTouchesInView = false
+        delegate = self
+    }
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        return !(touch.view is UIControl)
+    }
+}
+
+private class CameraPinchGestureRecognizer: UIPinchGestureRecognizer {}
